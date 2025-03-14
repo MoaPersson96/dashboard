e15e4613a25d04f0271b4983bb4706f986f6f47c
Benjamin Stenlund <benjamin.stenlund@chasacademy.se>
2025-03-14 14:03:48 +0100

    Style Mätare

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -5,21 +5,34 @@ export default function Dashboard() {
 
     return <main className="grid grid-cols-1 md:grid-cols-12 grid-flow-row-dense grid-rows-">
         <h1 className="md:col-span-full relative left-1/12">Dashboard</h1>
-        <section className="col-start-1 md:col-span-5 bg-blue-100">
+        <section className="p-2 col-start-1 md:col-span-5 bg-blue-100">
             <h2>Stapeldiagram</h2>
             <img/>
         </section>
-        <section className=" md:col-span-4 bg-blue-200">
+        <section className="p-2  md:col-span-4 bg-blue-200">
             <h2>Tårtdiagram</h2>
             <img/>
         </section>
-        <section className=" md:col-span-3 row-span-2 bg-pink-200">
+        <section className="p-2 md:col-span-3 row-span-2 bg-pink-200 flex flex-col items-center gap-6">
             <h2>Visare</h2>
-            <img/>
-            <img/>
-            <img/>
+
+            <div>
+                <div>Vinst</div>
+                <meter value="50" min="0" low="25" optimum="100" high="75" max="100"/>
+                <div>50%</div>
+            </div>
+            <div>
+                <div>Utsläpp</div>
+                <meter value="10" min="0" low="25" optimum="0" high="75" max="100"/>
+                <div>10%</div>
+            </div>
+            <div>
+                <div>Arbetsglädje</div>
+                <meter value="90" min="0" low="25" optimum="100" high="75" max="100"/>
+                <div>90%</div>
+            </div>
         </section>
-        <section className=" md:col-span-7 bg-blue-200">
+        <section className="p-2  md:col-span-7 bg-blue-200">
             <h2>Tabell</h2>
             <table>
                 <tr>
@@ -40,15 +53,15 @@ export default function Dashboard() {
                 </tr>
             </table>
         </section>
-        <section className=" md:col-span-2 bg-blue-100">
+        <section className="p-2  md:col-span-2 bg-blue-100">
             <h2>Mätare</h2>
             <section>
                 <h3>Tillbud</h3>
-                <span>0</span>
+                <span className="text-8xl text-green-500 font-mono">0</span>
             </section>
             <section>
                 <h3>Olyckor</h3>
-                <span>0</span>
+                <span className="text-8xl text-green-500 font-mono">0</span>
             </section>
         </section>
     </main>
