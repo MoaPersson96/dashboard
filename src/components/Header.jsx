1c7d6dc99f86e4bc88f14a49dc6517ebe500e12d
Benjamin Stenlund <benjamin.stenlund@chasacademy.se>
2025-03-14 13:44:30 +0100

    Grid for dashboard. Style header and footer.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,8 +3,8 @@ import Navbar from "./Navbar"
 export default function Header() {
 
 
-    return <header class="flex">
-        <span class="text-3xl font-bold italic">Dashbord app 1.0</span>
+    return <header className="flex justify-between flex-wrap pb-5">
+        <span className="text-2xl font-bold italic">Dashbord app 1.0</span>
         <Navbar/>
         <span>Användarnamn <a href="#">Logga ut</a></span>
     </header>
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -3,23 +3,23 @@
 export default function Dashboard() {
 
 
-    return <main>
-        <h1>Dashboard</h1>
-        <section>
+    return <main className="grid grid-cols-1 md:grid-cols-12 grid-flow-row-dense grid-rows-">
+        <h1 className="col-span-full relative left-1/12">Dashboard</h1>
+        <section className="col-start-1 col-span-5 bg-blue-100">
             <h2>Stapeldiagram</h2>
             <img/>
         </section>
-        <section>
+        <section className=" col-span-4 bg-blue-200">
             <h2>Tårtdiagram</h2>
             <img/>
         </section>
-        <section>
+        <section className=" col-span-3 row-span-2 bg-pink-200">
             <h2>Visare</h2>
             <img/>
             <img/>
             <img/>
         </section>
-        <section>
+        <section className=" col-span-7 bg-blue-200">
             <h2>Tabell</h2>
             <table>
                 <tr>
@@ -40,7 +40,7 @@ export default function Dashboard() {
                 </tr>
             </table>
         </section>
-        <section>
+        <section className=" col-span-2 bg-blue-100">
             <h2>Mätare</h2>
             <section>
                 <h3>Tillbud</h3>
